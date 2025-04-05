1dcca858846b742caa73cd0f86e65139b9b6df6a
Vinícius dos Reis Vieira <vinicius_vieira@sicredi.com.br>
2025-04-05 13:53:09 -0300

    final version supervided_ml

--- Inteligência Artificial/Atividades Avaliativas/3_ML Supervisionado/log_regression.py
+++ Inteligência Artificial/Atividades Avaliativas/3_ML Supervisionado/log_regression.py
@@ -1,20 +1,10 @@
 # %%
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-import matplotlib.pyplot as plt
 import time
-
-# path = r"C:\Users\vinicius_vieira\OneDrive - Sicredi\Residência IA\Códigos\Inteligência Artificial\Atividades Avaliativas\3_ML Supervisionado\data\dataset.csv"
-# df = pd.read_csv(path)
-
-# path_to_save = r"C:\Users\vinicius_vieira\OneDrive - Sicredi\Residência IA\Códigos\Inteligência Artificial\Atividades Avaliativas\3_ML Supervisionado\data\dataset.parquet"
-# df.to_parquet(path_to_save)
 
 path_parquet = r"C:\Users\vinicius_vieira\OneDrive - Sicredi\Residência IA\Códigos\Inteligência Artificial\Atividades Avaliativas\3_ML Supervisionado\data\dataset.parquet"
 df = pd.read_parquet(path_parquet)
@@ -34,11 +24,17 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Dicionário de modelos
 modelos = {
-    "Árvore de Decisão": DecisionTreeClassifier(),
-    "Random Forest": RandomForestClassifier(),
-    "Naive Bayes": GaussianNB(),
-    "Regressão Logística": LogisticRegression(),
+    "Regressão Logística Default": LogisticRegression(),
+    "Regressão Logística Mode 1": LogisticRegression(max_iter=200),
+    "Regressão Logística Mode 2": LogisticRegression(random_state=42, max_iter=800),
+    "Regressão Logística Mode 3": LogisticRegression(
+        random_state=42, max_iter=800, tol=1e-3
+    ),
+    "Regressão Logística Mode 4": LogisticRegression(
+        random_state=42, max_iter=1000, tol=1e-2
+    ),
 }
+
 
 # Tabela de resultados
 resultados = []
@@ -71,37 +67,13 @@ for nome, modelo in modelos.items():
             "Tempo de teste": round(test_time, 4),
         }
     )
-# Criar tabela comparativa
+# Criar tabela dos resultados
 df_resultados = pd.DataFrame(resultados)
 
 df_resultados.to_excel(
-    r"C:\Users\vinicius_vieira\OneDrive - Sicredi\Residência IA\Códigos\Inteligência Artificial\Atividades Avaliativas\3_ML Supervisionado\results\models_results.xlsx",
+    r"C:\Users\vinicius_vieira\OneDrive - Sicredi\Residência IA\Códigos\Inteligência Artificial\Atividades Avaliativas\3_ML Supervisionado\results\log_regression_result.xlsx",
     index=False,
 )
 
-# Visualização da comparação do desempenho dos modelos
-df_resultados.set_index("Modelo")[
-    ["Acurácia", "F1-Score", "Revocação", "Precisão"]
-].plot(kind="bar", figsize=(12, 6))
-plt.title("Comparação de Desempenho entre Modelos")
-plt.ylabel("Pontuação")
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-# Visualização da comparação do tempo entre os modelos
-df_resultados.set_index("Modelo")[
-    [
-        "Tempo de treinamento",
-        "Tempo de teste",
-    ]
-].plot(kind="bar", figsize=(12, 6))
-plt.title("Comparação de Tempos entre Modelos")
-plt.ylabel("Tempo")
-plt.ylim(-5, 100)
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-# Resultado do desempenho dos modelos
+# Resultado do desempenho
 df_resultados.head()
